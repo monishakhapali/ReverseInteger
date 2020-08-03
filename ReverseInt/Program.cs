c916ec43bc0dc97b0c6b9735ffad0495b2e37104
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-08-03 14:23:30 -0400

    Reversing an integer

---
+++ ReverseInt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 123;
+            string r = x.ToString();
+            string s = string.Empty;
+            for(int i= r.Length; i>=0;i--)
+            {
+                s = s + r[i];
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
